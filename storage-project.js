48090b99b434856d8bb5197fc733bab4348c6359
Andrew Moss <andrewmoss@Andrews-MacBook-Pro.local>
2021-02-04 14:33:05 -0600

    completed store item function -am

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,49 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-  // const showCart = () => {
 
-  // };
+  //!!                   SHOW CART ITEM                         //
 
-  // const storeItem = () => {
+  const showCart = () => {
 
-  // };
+
+
+
+  };
+
+  //!!                STORE ITEM FUNCTION                   //
+  const form = document.querySelector('.form.form__container')
+
+  const addButton = document.querySelector("#add-to-cart");
+  // console.log('addButton:', addButton)
+
+  const formItem = document.querySelector(".form__field-items")
+  // console.log('formItem:', formItem.value)
+
+  const formQuantity = document.querySelector(".form__field-quantity")
+
+  const storeItem = () => {
+    // console.log('formQuantity:', formQuantity.value)
+    //!!storing item that is say an ARRAY, you would JSON stringify into the value
+    form.addEventListener("submit", event => {
+      event.preventDefault();
+      let key = formItem.value
+      localStorage.setItem(key, formQuantity.value)
+      // console.log(localStorage.getItem("Apple"))
+
+    });
+
+
+
+  };
+
+
+
+  //!!                    REMOVE ITEM FUNCTION               //
 
   // const removeItem = () => {
 
   // };
 
+
+  storeItem();
 });
